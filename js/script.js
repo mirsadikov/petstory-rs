be94934adb5bbe5f0830def324547bc56fe9e2e2
Mirabror Sodikov <mirabror9545@gmail.com>
2022-11-01 18:58:06 +0500

    fix: requirements

---
+++ js/script.js
@@ -0,0 +1,11 @@
+document
+  .getElementById('testimonials-slider')
+  .addEventListener('change', function () {
+    alert('This is requirement of the WEEK 4');
+  });
+
+document.querySelectorAll('.pets__slider-btn').forEach((item) => {
+  item.addEventListener('click', function () {
+    alert('This is requirement of the WEEK 4');
+  });
+});
