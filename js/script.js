4224fb0c04d73bf837ad73832c61bdf11ae53679
Mirabror Sodikov <mirabror9545@gmail.com>
2022-12-05 22:58:10 +0500

    feat: added 'calltohelp' message

--- js/script.js
+++ js/script.js
@@ -9,3 +9,7 @@ document.querySelectorAll('.pets__slider-btn').forEach((item) => {
     alert('This is requirement of the WEEK 4');
   });
 });
+
+document.addEventListener('DOMContentLoaded', function () {
+  alert('Please give me some time to finish the project, I am working on it. Thank you!');
+});
