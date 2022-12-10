53f9a81cbe4521648f1121a757d6fd3aa93c892a
Mirabror Sodikov <mirabror9545@gmail.com>
2022-12-10 20:11:12 +0500

    feat: carousel

--- js/script.js
+++ js/script.js
@@ -1,15 +1,165 @@
-document
-  .getElementById('testimonials-slider')
-  .addEventListener('change', function () {
-    alert('This is requirement of the WEEK 4');
-  });
+const slider = document.querySelector('.pets__cards');
+const sliderPrev = document.querySelector('.pets__slider-btn--left');
+const sliderNext = document.querySelector('.pets__slider-btn--right');
 
-document.querySelectorAll('.pets__slider-btn').forEach((item) => {
-  item.addEventListener('click', function () {
-    alert('This is requirement of the WEEK 4');
-  });
-});
+const animalsJSON = [
+  {
+    img: '/img/panda.png',
+    title: 'Giant pandas',
+    desc: 'Native to Southwest China',
+    food: '/img/banana-bamboo_icon.svg',
+  },
+  {
+    img: '/img/eagle.png',
+    title: 'Eagles',
+    desc: 'Native to South America',
+    food: '/img/meet-fish_icon.svg',
+  },
+  {
+    img: '/img/gorilla.png',
+    title: 'Gorillas',
+    desc: 'Native to Congo',
+    food: '/img/banana-bamboo_icon.svg',
+  },
+  {
+    img: '/img/sloth.png',
+    title: 'Two-toed Sloth',
+    desc: 'Mesoamerica, South America',
+    food: '/img/banana-bamboo_icon.svg',
+  },
+  {
+    img: '/img/cheetah.png',
+    title: 'Cheetahs',
+    desc: 'Native to Africa',
+    food: '/img/meet-fish_icon.svg',
+  },
+  {
+    img: '/img/penguin.png',
+    title: 'Penguins',
+    desc: 'Native to Antarctica',
+    food: '/img/meet-fish_icon.svg',
+  },
+  {
+    img: '/img/gorilla2.png',
+    title: 'Gorillas',
+    desc: 'Native to Congo',
+    food: '/img/banana-bamboo_icon.svg',
+  },
+  {
+    img: '/img/alligator.png',
+    title: 'Alligators',
+    desc: 'Native to Southeastern United States',
+    food: '/img/meet-fish_icon.svg',
+  },
+  {
+    img: '/img/fox.png',
+    title: 'Foxes', 
+    desc: 'Native to North America',
+    food: '/img/meet-fish_icon.svg',
+  }
+];
+
+function checkSlideProps() {
+  if (window.innerWidth < 1000) {
+    slider.config = { slidesPerView: 4, gap: 3 };
+  } else if (window.innerWidth < 501) {
+    slider.config = { slidesPerView: 4, gap: 3 };
+  } else {
+    slider.config = { slidesPerView: 6, gap: 3 };
+  }
+}
+
+function arrayShuffle(o) {
+  for (
+    var j, x, i = o.length;
+    i;
+    j = parseInt(Math.random() * i), x = o[--i], o[i] = o[j], o[j] = x
+  );
+  return o;
+}
 
 document.addEventListener('DOMContentLoaded', function () {
-  alert('Please give me some time to finish the project, I am working on it. Thank you!');
+  checkSlideProps();
+  const { slidesPerView, gap } = slider.config;
+  renderNRandomAnimals(slidesPerView, 'beforeend');
+
+  sliderPrev.addEventListener('click', function () {
+    if (slider.animating) return;
+    slider.animating = true;
+    renderNRandomAnimals(slidesPerView, 'afterbegin');
+    slider.style.transform = `translateX(calc(-100% - ${gap}rem))`;
+    setTimeout(() => {
+      slider.style.transition = 'transform 0.5s ease-in-out';
+      slider.style.transform = 'translateX(0)';
+
+      setTimeout(() => {
+        slider.style.transition = 'none';
+
+        for (let i = 0; i < slidesPerView; i++) {
+          slider.removeChild(slider.lastElementChild);
+        }
+
+        slider.animating = false;
+      }, 500);
+    });
+  });
+
+  sliderNext.addEventListener('click', function () {
+    if (slider.animating) return;
+    slider.animating = true;
+    renderNRandomAnimals(slidesPerView, 'beforeend');
+    slider.style.transition = 'transform 0.5s ease-in-out';
+    slider.style.transform = `translateX(calc(-100% - ${gap}rem))`;
+
+    setTimeout(() => {
+      for (let i = 0; i < slidesPerView; i++) {
+        slider.removeChild(slider.firstElementChild);
+      }
+
+      slider.style.transition = 'none';
+      slider.style.transform = 'translateX(0)';
+      slider.animating = false;
+    }, 500);
+  });
 });
+
+function renderNRandomAnimals(n, place) {
+  arrayShuffle(animalsJSON)
+    .slice(0, n)
+    .forEach((animal) => {
+      const animalCard = document.createElement('a');
+      animalCard.classList.add('pets__card');
+      animalCard.href = '#';
+
+      const animalImg = document.createElement('img');
+      animalImg.classList.add('pets__card-img');
+      animalImg.src = animal.img;
+      animalImg.alt = animal.title;
+
+      const animalCap = document.createElement('div');
+      animalCap.classList.add('pets__card-cap');
+
+      const animalTitle = document.createElement('h4');
+      animalTitle.classList.add('pets__card-title', 'text-btn');
+      animalTitle.textContent = animal.title;
+
+      const animalDesc = document.createElement('p');
+      animalDesc.classList.add('pets__card-desc', 'text-small');
+      animalDesc.textContent = animal.desc;
+
+      const animalFood = document.createElement('img');
+      animalFood.classList.add('pets__card-food');
+      animalFood.src = animal.food;
+      animalFood.alt = animal.title;
+
+      animalCap.appendChild(animalTitle);
+      animalCap.appendChild(animalDesc);
+
+      animalCard.appendChild(animalImg);
+      animalCard.appendChild(animalCap);
+      animalCard.appendChild(animalFood);
+
+      // slider.appendChild(animalCard);
+      slider.insertAdjacentElement(place, animalCard);
+    });
+}
